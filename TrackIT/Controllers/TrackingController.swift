0cea1cdebca10243c7d74bc0f5f9a577a3ecf71c
mossab12 <mossabinfo@gmail.com>
2019-11-15 23:00:58 +0200

    first commit

---
+++ TrackIT/Controllers/TrackingController.swift
@@ -0,0 +1,20 @@
+//
+//  TrackingController.swift
+//  TrackIT
+//
+//  Created by Mac on 11/15/19.
+//  Copyright © 2019 mossab. All rights reserved.
+//
+
+import UIKit
+
+class TrackingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+}
---
+++ TrackIT/Services/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  TrackIT
+//
+//  Created by Mac on 11/15/19.
+//  Copyright © 2019 mossab. All rights reserved.
+//
+
+import Foundation
